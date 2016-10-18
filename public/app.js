e39b976e91e8a22248857f623106265b1e57b29e
Sahar Jafari <shr.jafari@gmail.com>
2016-10-17 18:05:43 -0700

    fix the issue

--- public/app.js
+++ public/app.js
@@ -54,6 +54,7 @@ function checkLoginState(event) {
     // User is signed-out of Facebook.
     firebase.auth().signOut();
     $('#status').html('Successfully logged out!');
+    $('.wrapper').css('display', 'none');
   }
 }
 
